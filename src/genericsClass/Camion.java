d6e8ed6686b62d662a278ae03590bdc83c18216c
Jose Figueroa <taodejoju@gmail.com>
2025-01-12 00:24:54 -0600

    Agregando parametro T a clases genericas

--- src/genericsClass/Camion.java
+++ src/genericsClass/Camion.java
@@ -6,20 +6,20 @@ import java.util.Iterator;
 import java.util.List;
 
 //Ejemplo de clases genericas
-public class Camion implements Iterable {
+public class Camion<T> implements Iterable<T> {
 
-    private List objetos;
+    private List<T> objetos;
     private int max;
     public Camion(int max) {
         this.max = max;
-        objetos = new ArrayList();
+        objetos = new ArrayList<>();
     }
     public Camion(List objetos, int max) {
         this.objetos = objetos;
         this.max = max;
     }
 
-    public void add(Object obj){
+    public void add(T obj){
         if(this.objetos.size() <= max) {
             this.objetos.add(obj);
         }
@@ -29,7 +29,7 @@ public class Camion implements Iterable {
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return this.objetos.iterator();
     }
 }
--- src/genericsClass/GenericsEjemplo.java
+++ src/genericsClass/GenericsEjemplo.java
@@ -4,6 +4,12 @@ public class GenericsEjemplo {
     public static void main(String[] args) {
 
         /*Ejemplo de Clases solo usando Objectos, esto condiciona el uso de casteo al querer instanciar un objeto diferente*/
+       /*
+       EJEMPLO USANDO TIPO OBJECT
+
+        */
+      /*
+
 
         Camion transporteCaballos = new Camion(5);
 
@@ -36,8 +42,41 @@ public class GenericsEjemplo {
         for (Object o : transporteAutomovil){
             Automovil movil = (Automovil) o;
             System.out.println("Marca: "+ movil.getMarca());
+        }*/
+        Camion<Animal> transporteCaballos = new Camion<>(5);
+
+        transporteCaballos.add(new Animal("Peregrino","caballo"));
+        transporteCaballos.add(new Animal("Cansino","caballo"));
+        transporteCaballos.add(new Animal("Raul","caballo"));
+        transporteCaballos.add(new Animal("Tornado","caballo"));
+        transporteCaballos.add(new Animal("Tornado","caballo"));
+
+         imprimir(transporteCaballos);
+        Camion<Maquinaria> transporteMaquinas = new Camion<>(3);
+        transporteMaquinas.add(new Maquinaria("Buldocer"));
+        transporteMaquinas.add(new Maquinaria("Grua Orquilla"));
+        transporteMaquinas.add(new Maquinaria("Perforadora"));
+
+        imprimir(transporteMaquinas);
+
+        Camion<Automovil> transporteAutomovil = new Camion<>(2);
+        transporteAutomovil.add(new Automovil("Toyota"));
+        transporteAutomovil.add(new Automovil("Honda"));
+
+        imprimir(transporteAutomovil);
+
         }
 
+        public static <T> void imprimir(Camion<T> generics){
+          for(T t:generics){
+              if(t instanceof Animal){
+                  System.out.println("Nombre: "+((Animal) t).getNombre() + " Tipo: "+((Animal) t).getTipo());
+              } else if (t instanceof  Automovil) {
+                  System.out.println("Marca : " + ((Automovil) t).getMarca());
+              } else if (t instanceof  Maquinaria) {
+                  System.out.println("Maquinaria : "+ ((Maquinaria) t).getTipo());
 
-    }
+              }
+          }
+        }
 }
